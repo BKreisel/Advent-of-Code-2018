b97ba88e1af24982655fbdeb1667e1607037b4a7
Brandon Kreisel <kreiselb@gmail.com>
2018-12-01 14:24:10 -0500

    Initial Commit: Day 1 Challenge 1

---
+++ README.md
@@ -0,0 +1,7 @@
+# Advent of Code 2018
+
+My attempt to solve the 2018 Advent of Code with a variety of languages.
+
+| Day                    | Challenge  | Language(s) |
+| -----------------------| -----------| ------------|
+| 1: Chronal Calibration | 1          | Python      | 
---
+++ python/day01_challenge1.py
@@ -0,0 +1,32 @@
+# Day 01
+import os.path
+
+
+INPUT_FILE = "day01_challenge1.txt"
+
+
+def open_input() -> str:
+    project_dir = os.path.split(os.path.split(os.path.abspath(__file__))[0])[0]
+    input_path = os.path.join(project_dir, "input", INPUT_FILE)
+    return open(input_path, "r")
+
+
+def main():
+    frequency: int = 0
+    with open_input() as lines:
+        for line in lines:
+            frequency += parse_frequency_change(line)
+    print(f"Frequency: {frequency}")
+
+
+def parse_frequency_change(line: str) -> int:
+    delta = int(line[1:])
+    if line[0] == "+":
+        return delta
+    if line[0] == "-":
+        return -delta
+    raise ValueError
+
+
+if __name__ == "__main__":
+    main()
