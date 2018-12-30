a6179173abb5e7440810b9574d8b8587bc983706
Brandon Kreisel <kreiselb@gmail.com>
2018-12-30 10:03:32 -0500

    Python: Day 9

--- README.md
+++ README.md
@@ -12,3 +12,4 @@ My attempt to solve the 2018 Advent of Code with a variety of languages.
 | 6: Chronal Coordinates                            | 1,2         | Python                                  |
 | 7: The Sum of Its Parts                           | 1,2         | Python                                  |
 | 8: Memory Maneuver                                | 1,2         | Python                                  |
+| 9: Marble Mania                                   | 1,2         | Python                                  |
---
+++ python/day09_challenge1.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+from util import read_input
+
+DAY = 9
+
+
+def main():
+    data: str = read_input(DAY)[0].split()
+    elf_count, last_marble = int(data[0]), int(data[6])
+    circle: deque = deque([0])
+    scores: dict = {}
+
+    for marble in range(1, last_marble + 1):
+        elf = (marble - 1) % elf_count + 1
+        if marble % 23 == 0:
+            circle.rotate(7)
+            score = circle.pop() + marble
+            scores[elf] = scores.get(elf, 0) + score
+            circle.rotate(-1)
+            continue
+        circle.rotate(-1)
+        circle.append(marble)
+        marble += 1
+
+    high_score = max(scores.values())
+    print(high_score)
+
+
+if __name__ == "__main__":
+    main()
